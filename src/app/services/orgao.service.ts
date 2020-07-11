ed244b7ff655cb9db78053f34f55870f0d70f16c
Evandro <efraibo@gmail.com>
2020-07-11 19:32:03 -0300

    Deploy heroku

--- src/app/services/orgao.service.ts
+++ src/app/services/orgao.service.ts
@@ -1,16 +1,18 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { Orgao } from "../models/orgao";
+import { environment } from "src/environments/environment";
 
 @Injectable({
   providedIn: "root",
 })
 export class OrgaoService {
-  private baseURL = "http://localhost:8080/api/v1/orgaos";
+  baseURL = environment.baseURL;
 
   constructor(private http: HttpClient) {}
 
   postEvento(orgaos: Orgao[]) {
+    let url = this.baseURL + "/api/v1/orgaos";
     return this.http.post(this.baseURL, orgaos);
   }
 }
