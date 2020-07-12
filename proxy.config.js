68a4142bd4a56b42a0db520b4bf49e5d5db871f0
Evandro <efraibo@gmail.com>
2020-07-12 15:56:21 -0300

    Deploy heroku

--- proxy.config.js
+++ proxy.config.js
@@ -2,6 +2,7 @@ const proxy = [{
   context: '/api',
   target: 'https://analyzing-technologies.herokuapp.com',
   secure: true,
+  //configuracao abaixo remove a api da URL
   // pathRewrite: {
   //   '^/api': ''
   // },
